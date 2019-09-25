c89f89d39f3f721e9d1841d2553802e758ed4ff2
harrydry2 <harrydry1996@gmail.com>
2019-09-25 11:37:25 +0100

    s

--- src/modules/mailPopup.js
+++ src/modules/mailPopup.js
@@ -84,7 +84,7 @@ export function mailPopup() {
         $('.outerMail').style.display = 'flex';
         $('.iosOverflow').classList.add('mailNoScroll');
         $('.iosOverflow').classList.add('number5chanel');
-      }, 60000);
+      }, 40000);
     } else {
       setTimeout(() => {
         document.addEventListener('mouseout', mouseoutFun);

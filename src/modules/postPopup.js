60e76f7611d96f72935a3e083395071f10206960
harrydry2 <harrydry1996@gmail.com>
2019-11-28 21:11:54 +0000

    s

--- src/modules/postPopup.js
+++ src/modules/postPopup.js
@@ -12,7 +12,7 @@ function closePopup() {
   $('.iosOverflow').classList.remove('postNoScroll');
   // push back to "/"
   window.history.pushState('', '', '/');
-  document.title = 'Marketing Examples | Really good marketing examples';
+  document.title = 'Marketing Examples | Real world marketing examples';
 }
 
 export function postPopup(cards) {

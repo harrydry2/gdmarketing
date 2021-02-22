5455530effd5aab0ab8a9af10f14283e9054869c
harrydry2 <harrydry1996@gmail.com>
2021-02-22 22:22:28 +0000

    s

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import "./sass/styles.sass";
-import axios from "axios";
-import { $, $$ } from "./modules/bling";
-import { postPopup } from "./modules/postPopup";
-import { resize, copyGif, gifStart } from "./modules/gifs";
-import { thScroll } from "./modules/thScroll";
-import { cycleImages } from "./modules/course";
+import './sass/styles.sass';
+import axios from 'axios';
+import { $, $$ } from './modules/bling';
+import { postPopup } from './modules/postPopup';
+import { resize, copyGif, gifStart } from './modules/gifs';
+import { thScroll } from './modules/thScroll';
+import { cycleImages } from './modules/course';
 import {
   mailPopup,
   mailSubmitFromPost,
   mailSubmitHome,
   mailSubmitFromHandbook,
   mailSubmitFromCourseTop,
-  mailSubmitFromCourseBottom
-} from "./modules/mailPopup";
-import { lazyLoad, gifLoad, gifLoadMobile } from "./modules/lazyLoad";
+  mailSubmitFromCourseBottom,
+} from './modules/mailPopup';
+import { lazyLoad, gifLoad, gifLoadMobile } from './modules/lazyLoad';
 
 window.page = 2;
 window.busy = false;
@@ -22,7 +22,7 @@ window.gifbusy = false;
 // 1) Deals with the filter across screens
 
 let filterArray;
-const array = Array.from($$(".filters__spec"));
+const array = Array.from($$('.filters__spec'));
 const arrayLength = array.length;
 
 if (window.innerWidth < 768) {
@@ -31,93 +31,93 @@ if (window.innerWidth < 768) {
   filterArray = array.slice(0, arrayLength / 2);
 }
 
-if (!$(".gif") && !$(".th") && !$(".cc")) {
+if (!$('.gif') && !$('.th') && !$('.cc')) {
   lazyLoad(filterArray);
 }
 
-if (!$(".gif") && !$(".th") && !$(".cc")) {
-  console.log("books");
+if (!$('.gif') && !$('.th') && !$('.cc')) {
+  console.log('books');
   filterArray.forEach(filterItem => {
-    filterItem.on("click", async e => {
+    filterItem.on('click', async e => {
       // reset page settings for new filters
       e.preventDefault();
       window.page = 2;
       window.busy = false;
       // toggle active state
-      e.currentTarget.classList.toggle("filters__active");
+      e.currentTarget.classList.toggle('filters__active');
       // array of active filters
       const activeFilters = filterArray
-        .filter(filter => filter.classList.contains("filters__active"))
+        .filter(filter => filter.classList.contains('filters__active'))
         .map(filter => filter.dataset.term)
-        .join("-");
+        .join('-');
       // if no filters default to all
       if (activeFilters.length === 0) {
-        window.history.pushState("", "", `/`);
+        window.history.pushState('', '', `/`);
         var { data } = await axios.get(`/api/lazy/1/all`);
       } else {
         // change push state
-        window.history.pushState("", "", `/${activeFilters}`);
+        window.history.pushState('', '', `/${activeFilters}`);
         // axios the filtered marketing ideas
         var { data } = await axios.get(`/api/lazy/1/${activeFilters}`);
       }
-      $(".outerCard").innerHTML = data;
+      $('.outerCard').innerHTML = data;
       // popup after dynamically inserted
-      postPopup(Array.from($$(".card")));
+      postPopup(Array.from($$('.card')));
     });
   });
 }
 
-if (!$(".gif") && !$(".th") && !$(".cc")) {
-  const hmButton = $(".hm__button");
-  const hmFilters = $(".hm__filters");
-  hmButton.on("click", () => {
-    const it1 = "Show filters";
-    const it2 = "Show marketing ideas";
+if (!$('.gif') && !$('.th') && !$('.cc')) {
+  const hmButton = $('.hm__button');
+  const hmFilters = $('.hm__filters');
+  hmButton.on('click', () => {
+    const it1 = 'Show filters';
+    const it2 = 'Show marketing ideas';
     if (hmButton.innerText === it1) {
       hmButton.innerText = it2;
     } else {
       hmButton.innerText = it1;
     }
-    hmFilters.classList.toggle("hm__filters-active");
-    $(".iosOverflow").classList.toggle("noScroll");
+    hmFilters.classList.toggle('hm__filters-active');
+    $('.iosOverflow').classList.toggle('noScroll');
   });
 }
 
 // mailPopup
-if (!$(".gif") && !$(".th") && !$(".cc")) {
-  if (!window.location.href.includes("utm_source=newsletter")) {
+if (!$('.gif') && !$('.th') && !$('.cc')) {
+  if (!window.location.href.includes('utm_source=newsletter')) {
     mailPopup();
   } else {
-    console.log("no more emails popups for loving subs");
+    console.log('no more emails popups for loving subs');
   }
   mailSubmitHome();
 }
 
 //
 
-if ($(".undera__center-text")) {
+if ($('.undera__center-text')) {
   if (window.innerWidth > 768) {
-    $(".undera__center-text").innerText = "CLICK ICON TO SHARE";
+    $('.undera__center-text').innerText = 'CLICK ICON TO SHARE';
   }
 }
 
 // only if Post page
-if ($(".postNoScroll")) {
+if ($('.postNoScroll')) {
   mailSubmitFromPost();
 }
 
 // popup from homepage (also if close post page)
-if (!$(".gif") && !$(".th") && !$(".cc")) {
-  postPopup(Array.from($$(".card")));
+if (!$('.gif') && !$('.th') && !$('.cc')) {
+  postPopup(Array.from($$('.card')));
 }
 
 // gif page
-if ($(".gif")) {
+if ($('.gif')) {
   mailPopup();
   copyGif();
   // if (window.innerWidth > 768) {
-  window.addEventListener("load", resize);
-  window.addEventListener("resize", resize);
+  window.addEventListener('load', resize);
+  window.addEventListener('resize', resize);
   gifLoad();
   // gifStart();
   // } else {
@@ -127,13 +127,18 @@ if ($(".gif")) {
 }
 
 // th page
-if ($(".th")) {
+if ($('.th')) {
   // mailPopup();
   thScroll();
   mailSubmitFromHandbook();
 }
 
-if ($(".cc")) {
+if ($('.cc')) {
   mailSubmitFromCourseTop();
-  mailSubmitFromCourseBottom();
+  setTimeout(() => {
+    const tc = $('.tweets__container');
+    console.log('hello');
+    tc.style.display = 'grid';
+    tc.style.height = 'auto';
+  }, 2000);
 }

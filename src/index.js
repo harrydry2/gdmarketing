4203dae46e66a8513cbf63a8544b68463690a423
harrydry2 <harrydry1996@gmail.com>
2021-06-08 16:32:03 +0100

    s

--- src/index.js
+++ src/index.js
@@ -222,4 +222,86 @@ if ($('.fed')) {
     cegMobileFilter();
     cegimobtap($$('.cegio'));
   }
+
+  // canvas setup
+  // const canvas = document.querySelector('canvas');
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
+  // const ctx = canvas.getContext('2d');
+
+  // // watch for browser resizing, reinitialize stars
+  // window.addEventListener('resize', function() {
+  //   canvas.width = window.innerWidth;
+  //   canvas.height = window.innerHeight;
+  //   init();
+  // });
+
+  // function Star(x, y, width, speed) {
+  //   this.x = x;
+  //   this.y = y;
+  //   this.width = width;
+  //   this.speed = speed;
+  //   this.color = '#fff';
+
+  //   this.draw = function() {
+  //     ctx.fillStyle = this.color;
+  //     ctx.fillRect(this.x, this.y, width, width);
+  //   };
+
+  //   this.update = () => {
+  //     // check bounds
+  //     if (this.y + this.width > innerWidth) {
+  //       this.y = 0;
+  //     }
+  //     this.y += this.speed;
+
+  //     this.draw();
+  //   };
+  // }
+
+  // // Star dimensions and speed
+  // const stars = {
+  //   midStar: {
+  //     width: 2,
+  //     speed: 0.6,
+  //   },
+  //   farStar: {
+  //     width: 1,
+  //     speed: 0.4,
+  //   },
+  // };
+
+  // let starArray = [];
+
+  // // clear starArray and generate 3 layers of stars randomly
+  // function init() {
+  //   starArray = [];
+  //   // nearest stars
+  //   // mid-distance stars
+  //   for (let i = 0; i < 200; ++i) {
+  //     const x = Math.random() * (innerWidth - stars.midStar.width);
+  //     const y = Math.random() * (innerHeight - stars.midStar.width);
+  //     starArray.push(new Star(x, y, stars.midStar.width, stars.midStar.speed));
+  //   }
+
+  //   // farthest stars
+  //   for (let i = 0; i < 400; ++i) {
+  //     const x = Math.random() * (innerWidth - stars.farStar.width);
+  //     const y = Math.random() * (innerHeight - stars.farStar.width);
+  //     starArray.push(new Star(x, y, stars.farStar.width, stars.farStar.speed));
+  //   }
+  // }
+
+  // // loop to call update function on each star
+  // function animate() {
+  //   requestAnimationFrame(animate);
+  //   ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  //   for (const star of starArray) {
+  //     star.update();
+  //   }
+  // }
+
+  // init();
+  // animate();
 }

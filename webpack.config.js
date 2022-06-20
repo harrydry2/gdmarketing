62138402ca96fce00d7c0d87fa4347c226f8e08f
harrydry2 <harrydry1996@gmail.com>
2022-06-20 11:31:53 +0100

    s

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public', 'dist'),
-    filename: 'bundle16.js'
+    filename: 'bundle17.js'
   },
   module: {
     rules: [
@@ -43,7 +43,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'bundle.v33.css',
+      filename: 'bundle.v34.css',
     }),
     // minify
     // new webpack.DefinePlugin({
